ba9c7473a35dc48574ba953d252049d34bea66e6
지인호 <77221262+key-del-jeeinho@users.noreply.github.com>
2022-05-31 10:57:01 +0900

    Initial commit

---
+++ src/test/kotlin/site/iplease/accountserver/AccountServerApplicationTests.kt
@@ -0,0 +1,13 @@
+package site.iplease.accountserver
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AccountServerApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
