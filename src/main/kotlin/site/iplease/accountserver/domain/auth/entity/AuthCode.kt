4a4b44f5c6680a945be35e16c946582558ae3f48
지인호 <77221262+key-del-jeeinho@users.noreply.github.com>
2022-06-01 09:18:10 +0900

    [ADD] 인증시 사용하는 엔티티 및 이에 필요한 타입객체 작성

---
+++ src/main/kotlin/site/iplease/accountserver/domain/auth/entity/AuthCode.kt
@@ -0,0 +1,9 @@
+package site.iplease.accountserver.domain.auth.entity
+
+import site.iplease.accountserver.domain.auth.data.type.AuthType
+
+data class AuthCode(
+    val code: String,
+    val type: AuthType,
+    val data: String
+)
