b4fb1b1fe2fe7592fc33f26d6cb49e82c515e3aa
지인호 <77221262+key-del-jeeinho@users.noreply.github.com>
2022-06-01 09:44:58 +0900

    [FIX] AuthCodeGenerator 명세추가 및 이에 따라 DummyAuthCodeGenerator 수정

--- src/main/kotlin/site/iplease/accountserver/domain/auth/util/AuthCodeGenerator.kt
+++ src/main/kotlin/site/iplease/accountserver/domain/auth/util/AuthCodeGenerator.kt
@@ -3,6 +3,5 @@ package site.iplease.accountserver.domain.auth.util
 import reactor.core.publisher.Mono
 
 interface AuthCodeGenerator {
-    fun generate(): Mono<String>
-
+    fun generate(): Mono<String> //현존하는 모든 인증코드는 고유해야한다.
 }
--- src/main/kotlin/site/iplease/accountserver/domain/auth/util/DummyAuthCodeGenerator.kt
+++ src/main/kotlin/site/iplease/accountserver/domain/auth/util/DummyAuthCodeGenerator.kt
@@ -3,8 +3,10 @@ package site.iplease.accountserver.domain.auth.util
 import org.springframework.stereotype.Component
 import reactor.core.publisher.Mono
 import reactor.kotlin.core.publisher.toMono
+import java.util.*
 
 @Component //TODO 실제 구현체 작성 후 제거 예정
 class DummyAuthCodeGenerator(): AuthCodeGenerator {
-    override fun generate(): Mono<String> = "인증코드입니다".toMono()
+    //현존하는 모든 인증코드는 고유해야한다.
+    override fun generate(): Mono<String> = UUID.randomUUID().toString().toMono()
 }
