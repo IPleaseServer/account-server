7b3a9c0f0efc062ede43d0a9450610d3320104ae
지인호 <77221262+key-del-jeeinho@users.noreply.github.com>
2022-06-01 09:18:57 +0900

    [ADD] 인증메일 발송 및 e2e테스트를 위해 thymeleaf, swagger 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -37,7 +37,10 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-mail")
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.boot:spring-boot-starter-webflux")
+    implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
     annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
+    //spring doc
+    implementation ("org.springdoc:springdoc-openapi-webflux-ui:1.6.8")
     //spring cloud
     implementation("org.springframework.cloud:spring-cloud-starter-config")
     implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client")
