c732b738be5f68cbf5fb81bc4685ded701fd89d8
지인호 <77221262+key-del-jeeinho@users.noreply.github.com>
2022-06-01 12:48:52 +0900

    [FIX] Service간의 정보전달을 위한 AuthDto 작성 및 적용

--- src/main/kotlin/site/iplease/accountserver/domain/auth/service/AuthorizeServiceImpl.kt
+++ src/main/kotlin/site/iplease/accountserver/domain/auth/service/AuthorizeServiceImpl.kt
@@ -7,6 +7,7 @@ import site.iplease.accountserver.domain.auth.data.dto.AuthTokenDto
 import site.iplease.accountserver.domain.auth.data.type.AuthType
 import site.iplease.accountserver.domain.auth.data.entity.AuthCode
 import site.iplease.accountserver.domain.auth.config.AuthProperties
+import site.iplease.accountserver.domain.auth.data.dto.AuthDto
 import site.iplease.accountserver.domain.auth.repository.AuthCodeRepository
 import site.iplease.accountserver.domain.auth.util.AuthCodeGenerator
 import site.iplease.accountserver.domain.auth.util.AuthTokenEncoder
@@ -26,15 +27,16 @@ class AuthorizeServiceImpl(
     override fun authorize(code: String): Mono<AuthTokenDto> =
         authCodeRepository.select(code) //DataStore에서 인증코드에 매핑된 Type(인증대상 종류 := Class)과 Data(인증대상 정보 := Instance)를 가져온다.
             .flatMap { deleteAuthData(it) }//DataStore에서 인증코드 매핑정보를 제거한다.
-            .flatMap { encodeAuthToken(it.type, it.data) }//가져온 Type과 Data로 인증토큰을 생성하고, 반환한다.
+            .flatMap { encodeAuthToken(AuthDto(it.type, it.data)) }//가져온 Type과 Data로 인증토큰을 생성하고, 반환한다.
 
-    private fun encodeAuthToken(type: AuthType, data: String): Mono<AuthTokenDto> =
-        authTokenEncoder.encodeAuthToken(type, data)//Type과 Data로 인증토큰을 생성한다.
+    private fun encodeAuthToken(dto: AuthDto): Mono<AuthTokenDto> =
+        authTokenEncoder.encode(dto)//Type과 Data로 인증토큰을 생성한다.
 
     override fun authorizeEmail(email: String): Mono<Unit> =
         authCodeGenerator.generate()//인증코드를 발급한다.
             .flatMap { sendAuthEmail(email, it) }//인증코드를 이메일로 보낸다.
-            .flatMap { saveAuthData(AuthType.EMAIL, email, it) }//인증코드와 이메일을 매핑하여 DataStore에 저장한다.
+            .map { AuthCode(it, AuthType.EMAIL, email) }//인증코드와 이메일을 매핑한다.
+            .flatMap { saveAuthData(it) }//매핑한 데이터를 DataStore에 저장한다.
             .map {  }//Payload가 비어있는 ResponseEntity(Status=OK)를 반환한다.
 
     private fun sendAuthEmail(email: String, authCode: String): Mono<String> =
@@ -46,15 +48,11 @@ class AuthorizeServiceImpl(
         ).map { authCode }//인증코드를 반환한다.
 
     ///Wrap Commands of AuthData to Private method
-    private fun saveAuthData(type: AuthType, data: String, code: String): Mono<String> =
-        authCodeRepository.insert(
-            AuthCode(//인증정보를 저장한다.
-            code = code,
-            type = type,
-            data = data)
-        ).then(code.toMono())//인증코드를 반환한다.
+    private fun saveAuthData(entity: AuthCode): Mono<String> =
+        authCodeRepository.insert(entity)//인증정보를 저장한다.
+            .then(entity.code.toMono())//인증코드를 반환한다.
 
-    private fun deleteAuthData(authCode: AuthCode) =
-        authCodeRepository.delete(code = authCode.code)//인증정보를 제거한다.
-            .then(authCode.toMono())//인증코드를 반환한다.
+    private fun deleteAuthData(entity: AuthCode) =
+        authCodeRepository.delete(code = entity.code)//인증정보를 제거한다.
+            .then(entity.toMono())//인증코드를 반환한다.
 }
--- src/main/kotlin/site/iplease/accountserver/domain/auth/util/JwtAuthTokenUtil.kt
+++ src/main/kotlin/site/iplease/accountserver/domain/auth/util/JwtAuthTokenUtil.kt
@@ -7,8 +7,8 @@ import org.springframework.stereotype.Component
 import reactor.core.publisher.Mono
 import reactor.kotlin.core.publisher.toMono
 import site.iplease.accountserver.domain.auth.config.AuthProperties
+import site.iplease.accountserver.domain.auth.data.dto.AuthDto
 import site.iplease.accountserver.domain.auth.data.dto.AuthTokenDto
-import site.iplease.accountserver.domain.auth.data.type.AuthType
 import java.sql.Timestamp
 import java.time.LocalDateTime
 
@@ -18,15 +18,15 @@ class JwtAuthTokenUtil(
 ): AuthTokenEncoder {
     val jwtProperties = lazy { authProperties.jwtProperties }
 
-    override fun encodeAuthToken(type: AuthType, data: String): Mono<AuthTokenDto> =
+    override fun encode(dto: AuthDto): Mono<AuthTokenDto> =
         LocalDateTime.now().toMono().map {
             Jwts.builder()
                 .setHeaderParam(Header.TYPE, Header.JWT_TYPE)
                 .setIssuer(jwtProperties.value.issuer)
                 .setIssuedAt(Timestamp.valueOf(it))
                 .setExpiration(Timestamp.valueOf(it.plusSeconds(jwtProperties.value.expireSecond)))
-                .claim("type", type)
-                .claim("data", data)
+                .claim("type", dto.type)
+                .claim("data", dto.data)
                 .signWith(SignatureAlgorithm.HS256, jwtProperties.value.secret)
                 .compact()
         }.map { AuthTokenDto(token = it) }
