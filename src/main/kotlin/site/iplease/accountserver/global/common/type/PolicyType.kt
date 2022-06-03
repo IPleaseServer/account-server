2fffab0ac49e59ad32f069714a3b9115bbf8f554
지인호 <77221262+key-del-jeeinho@users.noreply.github.com>
2022-06-03 20:58:29 +0900

    [ADD] PolicyType에 로그인 관련 정책분류 추가

--- src/main/kotlin/site/iplease/accountserver/global/common/type/PolicyType.kt
+++ src/main/kotlin/site/iplease/accountserver/global/common/type/PolicyType.kt
@@ -1,6 +1,8 @@
 package site.iplease.accountserver.global.common.type
 
 enum class PolicyType(val displayName: String) {
+    TEACHER_CODE("교사 인증코드"),
     REGISTER_EMAIL("회원가입 이메일"), REGISTER_STUDENT_NUMBER("회원가입 학번"),
-    TEACHER_CODE("교사 인증코드")
+    LOGIN_EMAIL("로그인 이메일"), LOGIN_PASSWORD("로그인 비밀번호"),
+    LOGIN_REFRESH_TOKEN("로그인 재발급 토큰"),
 }
