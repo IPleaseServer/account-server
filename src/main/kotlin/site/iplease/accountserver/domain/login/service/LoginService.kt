9edad3d305e13b790a4f1e05945381182f035af4
지인호 <77221262+key-del-jeeinho@users.noreply.github.com>
2022-06-03 14:25:38 +0900

    [ADD] LoginController 구현 및 이에 필요한 Component 작성
    - 로그인 로직 전반을 담당하는 LoginService 작성
    - Controller에서 사용하는 Response/Request Data 작성
    - Service에서 사용하는 Dto작성

---
+++ src/main/kotlin/site/iplease/accountserver/domain/login/service/LoginService.kt
@@ -0,0 +1,9 @@
+package site.iplease.accountserver.domain.login.service
+
+import reactor.core.publisher.Mono
+import site.iplease.accountserver.domain.login.data.dto.LoginTokenDto
+
+interface LoginService {
+    fun login(email: String, password: String): Mono<LoginTokenDto>
+    fun refreshLogin(refreshToken: String): Mono<LoginTokenDto>
+}
