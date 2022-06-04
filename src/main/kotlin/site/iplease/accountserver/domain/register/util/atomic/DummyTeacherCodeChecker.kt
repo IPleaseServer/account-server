eb0097137c68df676ce7ce5b214ae85b8e2886fe
지인호 <77221262+key-del-jeeinho@users.noreply.github.com>
2022-06-04 14:56:10 +0900

    [ADD] TeacherCodeChecker 구현 (Dummy컴포넌트 비활성화)

--- src/main/kotlin/site/iplease/accountserver/domain/register/util/atomic/DummyTeacherCodeChecker.kt
+++ src/main/kotlin/site/iplease/accountserver/domain/register/util/atomic/DummyTeacherCodeChecker.kt
@@ -1,12 +1,10 @@
 package site.iplease.accountserver.domain.register.util.atomic
 
-import org.springframework.stereotype.Component
 import reactor.core.publisher.Mono
 import reactor.kotlin.core.publisher.toMono
 import site.iplease.accountserver.global.common.type.PolicyType
 import site.iplease.accountserver.global.common.exception.PolicyViolationException
 
-@Component
 class DummyTeacherCodeChecker: TeacherCodeChecker {
     override fun valid(teacherCode: String): Mono<Unit> =
         if(teacherCode.startsWith("teacherCode")) Unit.toMono()
