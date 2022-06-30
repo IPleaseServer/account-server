bb9d92f3f15295630c812262ac1d6fd766306e4c
raul <develop.raul@gmail.com>
2022-06-30 20:49:37 +0900

    [REMOVE] 사용하지 않는 DepartmentType Constant 제거

--- src/main/kotlin/site/iplease/accountserver/global/common/type/DepartmentType.kt
+++ src/main/kotlin/site/iplease/accountserver/global/common/type/DepartmentType.kt
@@ -3,7 +3,7 @@ package site.iplease.accountserver.global.common.type
 enum class DepartmentType(
     vararg clazz: Int
 ) {
-    SMART_IOT(3, 4), SOFTWARE_DEVELOP(1, 2), UNKNOWN;
+    SMART_IOT(3, 4), SOFTWARE_DEVELOP(1, 2);
 
     val classes = clazz.toSet()
 }
