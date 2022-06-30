3086767a8931765e7c2b7c8d9c36d41d231d531f
raul <develop.raul@gmail.com>
2022-06-30 20:19:45 +0900

    [FIX] ProfileService에 ProfileConverter적용

--- src/main/kotlin/site/iplease/accountserver/domain/profile/service/ProfileServiceImpl.kt
+++ src/main/kotlin/site/iplease/accountserver/domain/profile/service/ProfileServiceImpl.kt
@@ -3,25 +3,25 @@ package site.iplease.accountserver.domain.profile.service
 import org.springframework.stereotype.Service
 import reactor.core.publisher.Mono
 import site.iplease.accountserver.domain.profile.data.dto.ProfileDto
-import site.iplease.accountserver.global.common.entity.Account
+import site.iplease.accountserver.domain.profile.util.ProfileConverter
 import site.iplease.accountserver.global.common.exception.UnknownAccountException
 import site.iplease.accountserver.global.login.util.atomic.AccessTokenDecoder
 import site.iplease.accountserver.global.common.repository.AccountRepository
-import java.net.URI
 
 @Service
 class ProfileServiceImpl(
     private val accountRepository: AccountRepository,
-    private val accessTokenDecoder: AccessTokenDecoder
+    private val accessTokenDecoder: AccessTokenDecoder,
+    private val profileConverter: ProfileConverter
 ): ProfileService {
     override fun getProfileByAccessToken(accessToken: String): Mono<ProfileDto> =
         accessTokenDecoder.decode(accessToken)//액세스토큰을 검증하여, accountId를 가져온다.
             .flatMap { getAccountByAccountId(it) }//accountId를 통해 계정의 프로필을 가져온다.
-            .map { it.toProfileDto() }//프로필을 반환한다.
+            .flatMap { account -> profileConverter.toDto(account) }//프로필을 반환한다.
 
     override fun getProfileByAccountId(accountId: Long): Mono<ProfileDto> =
         getAccountByAccountId(accountId)//accountId를 통해 계정의 프로필을 가져온다.
-            .map { it.toProfileDto() }//프로필을 반환한다.
+            .flatMap { account -> profileConverter.toDto(account) }//프로필을 반환한다.
 
     override fun existProfileByAccessToken(accessToken: String): Mono<Boolean> =
         accessTokenDecoder.decode(accessToken)
@@ -37,11 +37,4 @@ class ProfileServiceImpl(
 
     private fun existAccountByAccountId(accountId: Long) =
         accountRepository.existsById(accountId)
-
-    private fun Account.toProfileDto() =
-        ProfileDto(
-            type = type, accountId = id, permission = permission,
-            name = name, email = email, profileImage = URI("127.0.0.1"), //TODO Account에 ProfileImage관련 로직 추가
-            studentNumber = studentNumber, department = department
-        )
 }
