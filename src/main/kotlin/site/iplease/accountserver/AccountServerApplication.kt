1c75f3c4f91e48b16d0428759d744c5ef4adec54
raul <develop.raul@gmail.com>
2022-08-12 16:24:53 +0900

    [ADD] EmailProperty 작성 및 적용

--- src/main/kotlin/site/iplease/accountserver/AccountServerApplication.kt
+++ src/main/kotlin/site/iplease/accountserver/AccountServerApplication.kt
@@ -6,9 +6,10 @@ import org.springframework.boot.runApplication
 import site.iplease.accountserver.domain.auth.config.AuthProperties
 import site.iplease.accountserver.domain.login.config.LoginProperties
 import site.iplease.accountserver.domain.profile.config.ProfileImageProperties
+import site.iplease.accountserver.infra.email.config.EmailProperty
 
 @SpringBootApplication
-@EnableConfigurationProperties(AuthProperties::class, LoginProperties::class, ProfileImageProperties::class)
+@EnableConfigurationProperties(AuthProperties::class, LoginProperties::class, ProfileImageProperties::class, EmailProperty::class)
 class AccountServerApplication
 
 fun main(args: Array<String>) {
