cd2a2886894be2ca5f675d9011033b40100578e7
raul <develop.raul@gmail.com>
2022-08-13 16:15:57 +0900

    [FIX] Request 내 사용하지 않을 Validation Annotation 제거

--- src/main/kotlin/site/iplease/accountserver/domain/register/data/request/StudentRegisterRequest.kt
+++ src/main/kotlin/site/iplease/accountserver/domain/register/data/request/StudentRegisterRequest.kt
@@ -1,15 +1,11 @@
 package site.iplease.accountserver.domain.register.data.request
 
-import org.hibernate.validator.constraints.Length
 import site.iplease.accountserver.global.common.type.DepartmentType
-import javax.validation.constraints.Pattern
 
 data class StudentRegisterRequest (
-    @field:Length(min = 2, max = 5)
     val name: String,
     val emailToken: String,
     val password: String,
-    @field:Pattern(regexp = "[0-3][1-4][0-2][0-9]")
     val studentNumber: String,
     val department: DepartmentType
 )
