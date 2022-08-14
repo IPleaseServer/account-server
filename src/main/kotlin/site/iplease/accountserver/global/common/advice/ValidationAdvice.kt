43ed82f0acc8618a0381f4b9963157e2a4e74865
raul <develop.raul@gmail.com>
2022-08-14 11:46:06 +0900

    [FIX] RuleAdvice 및 PolicyAdvice 병합

--- src/main/kotlin/site/iplease/accountserver/global/common/advice/ValidationAdvice.kt
+++ src/main/kotlin/site/iplease/accountserver/global/common/advice/ValidationAdvice.kt
@@ -4,12 +4,22 @@ import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
 import reactor.kotlin.core.publisher.toMono
+import site.iplease.accountserver.global.common.exception.PolicyViolationException
 import site.iplease.accountserver.global.common.exception.RuleViolationException
 import site.iplease.accountserver.global.error.ErrorResponse
 import site.iplease.accountserver.global.error.ErrorStatus
 
 @RestControllerAdvice
-class RuleAdvice {
+class ValidationAdvice {
+    @ExceptionHandler(PolicyViolationException::class)
+    fun handle(e: PolicyViolationException) =
+        ResponseEntity.badRequest().body(
+            ErrorResponse(
+                status = ErrorStatus.POLICY_VIOLATION_ERROR,
+                message = e.getErrorMessage(),
+                detail = e.getErrorDetail()
+            )).toMono()
+    
     @ExceptionHandler(RuleViolationException::class)
     fun handle(e: RuleViolationException) =
         ResponseEntity.badRequest().body(
