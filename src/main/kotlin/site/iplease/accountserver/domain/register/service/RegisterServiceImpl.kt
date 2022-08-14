b04ef73caa62dd30bda7a9002c937dbd3ea88ca9
raul <develop.raul@gmail.com>
2022-08-14 11:42:14 +0900

    [ADD] RegisterServiceImpl 구현체 작성

---
+++ src/main/kotlin/site/iplease/accountserver/domain/register/service/RegisterServiceImpl.kt
@@ -0,0 +1,20 @@
+package site.iplease.accountserver.domain.register.service
+
+import org.springframework.stereotype.Service
+import reactor.core.publisher.Mono
+import site.iplease.accountserver.domain.register.data.dto.StudentDto
+import site.iplease.accountserver.domain.register.data.dto.StudentRegistrationDto
+import site.iplease.accountserver.domain.register.util.RegisterConverter
+import site.iplease.accountserver.global.common.repository.AccountRepository
+
+@Service
+class RegisterServiceImpl(
+    private val registerConverter: RegisterConverter,
+    private val accountRepository: AccountRepository
+): RegisterService {
+    override fun registerStudent(dto: StudentRegistrationDto): Mono<StudentDto> =
+        registerConverter.toEntity(dto)
+            .map { it.copy(id=0) }
+            .flatMap { newEntity -> accountRepository.save(newEntity) }
+            .flatMap { savedEntity -> registerConverter.toStudentDto(savedEntity) }
+}
