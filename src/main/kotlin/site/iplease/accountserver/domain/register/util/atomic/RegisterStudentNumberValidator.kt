c226b3a31690ba9026d61124518c21e7f0c55904
raul <develop.raul@gmail.com>
2022-08-14 13:14:49 +0900

    [REMOVE] 사용하지 않는 레거시 제거

--- src/main/kotlin/site/iplease/accountserver/domain/register/util/atomic/RegisterStudentNumberValidator.kt
+++
@@ -1,7 +0,0 @@
-package site.iplease.accountserver.domain.register.util.atomic
-
-import reactor.core.publisher.Mono
-
-interface RegisterStudentNumberValidator {
-    fun valid(studentNumber: Int): Mono<Unit>
-}
