36b73125b5a88f95944dacd545b71ce2210c2744
raul <develop.raul@gmail.com>
2022-08-14 11:40:36 +0900

    [FIX] RegisterConverter스팩 추가 및 변경

--- src/main/kotlin/site/iplease/accountserver/domain/register/util/RegisterConverter.kt
+++ src/main/kotlin/site/iplease/accountserver/domain/register/util/RegisterConverter.kt
@@ -1,17 +1,18 @@
 package site.iplease.accountserver.domain.register.util
 
 import reactor.core.publisher.Mono
-import site.iplease.accountserver.domain.register.data.dto.StudentRegisterValidatedDto
+import site.iplease.accountserver.domain.register.data.dto.StudentDto
 import site.iplease.accountserver.domain.register.data.dto.StudentRegisterValidationDto
+import site.iplease.accountserver.domain.register.data.dto.StudentRegistrationDto
 import site.iplease.accountserver.domain.register.data.event.StudentRegisteredEvent
 import site.iplease.accountserver.domain.register.data.request.StudentRegisterRequest
 import site.iplease.accountserver.domain.register.data.response.RegisterResponse
-import site.iplease.accountserver.domain.register.dto.StudentRegistrationDto
+import site.iplease.accountserver.global.common.entity.Account
 
 interface RegisterConverter {
     fun toValidationDto(request: StudentRegisterRequest): Mono<StudentRegisterValidationDto>
-    fun toDto(validationDto: StudentRegisterValidatedDto): Mono<StudentRegistrationDto>
-    fun toStudentRegisteredEvent(dto: StudentRegistrationDto): Mono<StudentRegisteredEvent>
-    fun toRegisterResponse(dto: StudentRegistrationDto): Mono<RegisterResponse>
-
+    fun toStudentRegisteredEvent(dto: StudentDto): Mono<StudentRegisteredEvent>
+    fun toRegisterResponse(dto: StudentDto): Mono<RegisterResponse>
+    fun toEntity(dto: StudentRegistrationDto): Mono<Account>
+    fun toStudentDto(entity: Account): Mono<StudentDto>
 }
