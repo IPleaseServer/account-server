e416e14d17cb8f1e35d4a83cceb018dbcd03b3a3
raul <develop.raul@gmail.com>
2022-08-31 10:57:11 +0900

    프로필 수정시 이메일 인증 단게 제거

--- src/main/kotlin/site/iplease/accountserver/domain/profile/util/ProfileCommandPreprocessorImpl.kt
+++ src/main/kotlin/site/iplease/accountserver/domain/profile/util/ProfileCommandPreprocessorImpl.kt
@@ -30,14 +30,14 @@ class ProfileCommandPreprocessorImpl(
 
     //프로필 업데이트 정책을 검사한다.
     override fun validate(accountId: Long, request: UpdateProfileRequest): Mono<Unit> =
-        //이메일 토큰을 decode한다.
-        authTokenDecoder.decode(AuthTokenDto(token = request.emailToken))
-            //decode해서 얻은 인증토큰을 validate한다. (인증토큰이 유효한 이메일 토큰인가)
-            .flatMap { dto -> emailTokenValidator.validate(dto).map { dto.data } }
-            //decode해서 얻은 email을 validate한다. (요청자가 해당email의 소유자인가)
-            .flatMap { email -> accountOwnerValidator.validate(accountId, email).map { email } }
+//        //이메일 토큰을 decode한다.
+//        authTokenDecoder.decode(AuthTokenDto(token = request.emailToken))
+//            //decode해서 얻은 인증토큰을 validate한다. (인증토큰이 유효한 이메일 토큰인가)
+//            .flatMap { dto -> emailTokenValidator.validate(dto).map { dto.data } }
+//            //decode해서 얻은 email을 validate한다. (요청자가 해당email의 소유자인가)
+//            .flatMap { email -> accountOwnerValidator.validate(accountId, email).map { email } }
             //요청자 ID, 요청정보를 변경될 프로필에 필요한 부가정보로 convert 한다.
-            .flatMap { profileChangeInfoConverter.convert(accountId, request) }
+            /*.flatMap {*/ profileChangeInfoConverter.convert(accountId, request) //}
             //ProfileDto 구성에 필요한 부가정보와 요청정보를 ProfileDto로 convert한다.
             .flatMap { profileConverter.toDto(request, it.first, it.second) }
             //convert해서 얻은 프로필Dto를 validate한다. (프로필 정보가 유효한가)
